8b4dcfecdb28cd8b4a2cebee3b34c12b99ceb0ee
pranavaselva <145452022+pranavaselva@users.noreply.github.com>
2024-01-08 14:29:53 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+// Progression 1: create a consant "NEG_INF" with -1000000 value, and a function createPop
+
+// Progression 2: add 3 variables: title, currIndex, check
+
+// Progression 3: create a function "searchForElement"
+
+// Progression 4: In `createPop()`, return a function - which uses the updated values of `currIndex` and `check`
+// to return the desired output.
+
+const arrayOfNumbers = [1, 2, 3, 4, 5, 6];
+const itemToSearch = 4;
+
+const NEG_INF = -1000000;
+function createPop(arrayOfNumbers, itemToSearch) {
+  const title = document.createElement('h3');
+  let currIndex = NEG_INF;
+  let check = false;
+
+  return function find() {
+    arrayOfNumbers.forEach(function (element, index) {
+      if (element == itemToSearch) {
+        currIndex = index;
+        check = true;
+      }
+    });
+    if (check) {
+      return `The item is present and is at index ${currIndex}`;
+    } else {
+      return `The item is not present and is at index ${currIndex}`;
+    }
+  };
+}
